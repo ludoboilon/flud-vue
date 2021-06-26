1de38f949d84ed91bc6008f3a6cfbe53eb0ba8b9
ludoboilon <boilon.ludovic@protonmail.com>
2021-06-26 15:14:17 +0200

    First commit !

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// const products = [
+//   { id: 1, name: "Camel 190g", prixLux: 22.6, prixFr: 10 },
+//   { id: 2, name: "Camel 10x30g", prixLux: 41.5, prixFr: 14 },
+//   { id: 3, name: "Fleur du Pays 10x50g", prixLux: 57.5, prixFr: 24 },
+//   { id: 4, name: "Fleur du Pays 200g", prixLux: 23, prixFr: 10 },
+//   { id: 5, name: "Interval 200g", prixLux: 25, prixFr: 10 },
+//   { id: 6, name: "Lucky Strike 10x50g", prixLux: 69, prixFr: 19 },
+//   { id: 7, name: "Lucky Strike 150g", prixLux: 16.2, prixFr: 69 },
+//   { id: 8, name: "Winston 8x30g", prixLux: 52.4, prixFr: 112 },
+//   { id: 9, name: "Winston 170g", prixLux: 19.6, prixFr: 80 },
+//   { id: 10, name: "Winston TUBE 150g", prixLux: 16.5, prixFr: 70 }
+// ];
+
+// const totalDisplay = document.getElementById("total-cart");
+
+// // let totalCart = 0;
+// // totalDisplay.innerText = totalCart;
