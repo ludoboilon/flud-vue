c814b4d36cd1a23205d3da7fa8ed38aeca9a9441
ludoboilon <boilon.ludovic@protonmail.com>
2021-06-26 20:25:43 +0200

    Add reduction

--- js/vue.js
+++ js/vue.js
@@ -3,16 +3,16 @@ const vm = Vue.createApp({
     return {
       total: 0,
       products: [
-        { id: 1, name: "Camel 190g", prixLux: 22.6, prixFr: 100, quantity:0, subtotal:0 },
-        { id: 2, name: "Camel 10x30g", prixLux: 41.5, prixFr: 140, quantity:0, subtotal:0 },
-        { id: 3, name: "Fleur du Pays 10x50g", prixLux: 57.5, prixFr: 240, quantity:0, subtotal:0 },
-        { id: 4, name: "Fleur du Pays 200g", prixLux: 23, prixFr: 100, quantity:0, subtotal:0 },
-        { id: 5, name: "Interval 200g", prixLux: 25, prixFr: 100, quantity:0, subtotal:0 },
-        { id: 6, name: "Lucky Strike 10x50g", prixLux: 69, prixFr: 190, quantity:0, subtotal:0 },
-        { id: 7, name: "Lucky Strike 150g", prixLux: 16.2, prixFr: 69, quantity:0, subtotal:0 },
-        { id: 8, name: "Winston 8x30g", prixLux: 52.4, prixFr: 112, quantity:0, subtotal:0 },
-        { id: 9, name: "Winston 170g", prixLux: 19.6, prixFr: 80, quantity:0, subtotal:0 },
-        { id: 10, name: "Winston TUBE 150g", prixLux: 16.5, prixFr: 70, quantity:0, subtotal:0 }
+        { id: 1, name: "Camel 190g", prixLux: 22.6, prixFr: 75, quantity:0, subtotal:0 },
+        { id: 2, name: "Camel 10x30g", prixLux: 41.5, prixFr: 105, quantity:0, subtotal:0 },
+        { id: 3, name: "Fleur du Pays 10x50g", prixLux: 57.5, prixFr: 180, quantity:0, subtotal:0 },
+        { id: 4, name: "Fleur du Pays 200g", prixLux: 23, prixFr: 70, quantity:0, subtotal:0 },
+        { id: 5, name: "Interval 200g", prixLux: 25, prixFr: 70, quantity:0, subtotal:0 },
+        { id: 6, name: "Lucky Strike 10x50g", prixLux: 69, prixFr: 140, quantity:0, subtotal:0 },
+        { id: 7, name: "Lucky Strike 150g", prixLux: 16.2, prixFr: 57.5, quantity:0, subtotal:0 },
+        { id: 8, name: "Winston 8x30g", prixLux: 52.4, prixFr: 85, quantity:0, subtotal:0 },
+        { id: 9, name: "Winston 170g", prixLux: 19.6, prixFr: 60, quantity:0, subtotal:0 },
+        { id: 10, name: "Winston TUBE 150g", prixLux: 16.5, prixFr: 57.5, quantity:0, subtotal:0 }
       ],
     }
   },
@@ -21,16 +21,4 @@ const vm = Vue.createApp({
       console.log('hello world!');
     },
   },
-  computed: {
-    subtotalCalc() {
-      for(i = 0; i < products.length; i++) {
-        this.subtotal = this.quantity * this.prixLux;
-      }
-    },
-    totalCart() {
-      for(i = 0; i < products.length; i++) {
-        total += products[i].subtotal;
-      }
-    }
-  }
 }).mount('#app');
